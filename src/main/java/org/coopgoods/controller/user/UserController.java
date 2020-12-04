c967ca1cbb0e0275d12774ab7217121962f8a255
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-04 17:34:44 +0100

    init find all

--- src/main/java/org/coopgoods/controller/user/UserController.java
+++ src/main/java/org/coopgoods/controller/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.coopgoods.api.UserAPI;
 import org.coopgoods.boot.TownHallConfiguration;
 import org.coopgoods.model.user.User;
+import org.coopgoods.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController implements UserAPI {
 
   @Autowired private TownHallConfiguration configuration;
+  @Autowired private UserService userService;
 
   @Override
   public List<User> findAll() {
-    return null;
+    return userService.findAll();
   }
 }
--- src/main/java/org/coopgoods/service/user/UserService.java
+++ src/main/java/org/coopgoods/service/user/UserService.java
@@ -1,6 +1,9 @@
 package org.coopgoods.service.user;
 
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
+import org.coopgoods.model.user.User;
 import org.coopgoods.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,8 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserService {
   @Autowired private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
 }
